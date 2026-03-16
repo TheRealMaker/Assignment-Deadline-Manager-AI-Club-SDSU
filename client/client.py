fda7a26ebf2056ecff08711d383e338ebf97cbb4
trife0907 <alecbuhr@gmail.com>
2026-03-16 15:30:47 -0700

    Edited client.py

---
+++ client/client.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+root = tk.Tk()
+
+# Set window properties
+root.title("Assignment Deadline Manager")
+root.minsize(200, 200)
+root.geometry("300x300+0+0")
+
+root.mainloop()
