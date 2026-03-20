dddd5f50d0cc7b5e636e7073cd31d9c9afa10909
trife0907 <alecbuhr@gmail.com>
2026-03-20 14:34:53 -0700

    Button/token login testing

--- client/client.py
+++ client/client.py
@@ -1,15 +1,27 @@
-# import tkinter as tk
-
-# root = tk.Tk()
-
-# # Set window properties
-# root.title("Assignment Deadline Manager")
-# root.minsize(200, 200)
-# root.geometry("300x300++0")
-
-# root.mainloop()
-
 import customtkinter
 
+# Generates a basic app
 app = customtkinter.CTk()
+
+# Window resolution
+app.geometry("400x300")
+
+secret_token = None
+
+# button 
+def button_click_event():
+    global secret_token
+    dialog = customtkinter.CTkInputDialog(text="Type in your in your token below:", title="Test")
+    secret_token = dialog.get_input()
+    print("You are now logged in: ", secret_token)
+
+
+# Button that allows for inputting login token
+button = customtkinter.CTkButton(app, text="Open Login Request", command=button_click_event)
+button.pack(padx=20, pady=20)
+
+print("You are now logged in: ", secret_token)
+
+
+
 app.mainloop()
