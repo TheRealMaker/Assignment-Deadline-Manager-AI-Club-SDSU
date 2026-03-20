559de28408c7f06cdd41cd67771a3f6c400b780b
trife0907 <alecbuhr@gmail.com>
2026-03-20 13:47:19 -0700

    Random push for Matt

--- client/client.py
+++ client/client.py
@@ -1,10 +1,15 @@
-import tkinter as tk
+# import tkinter as tk
 
-root = tk.Tk()
+# root = tk.Tk()
 
-# Set window properties
-root.title("Assignment Deadline Manager")
-root.minsize(200, 200)
-root.geometry("300x300+0+0")
+# # Set window properties
+# root.title("Assignment Deadline Manager")
+# root.minsize(200, 200)
+# root.geometry("300x300++0")
 
-root.mainloop()
+# root.mainloop()
+
+import customtkinter
+
+app = customtkinter.CTk()
+app.mainloop()
